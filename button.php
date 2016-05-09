1e6bf0cf55000f5c1ee381ee4959990c0892d037
Binh Dang <bdang2012@gmail.com>
2016-05-09 17:13:10 -0400

    initil commit

---
+++ button.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+
+<body>
+<h1> Ann Louise Messenger Test </h1>
+
+
+<!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en" target="_blank" onclick="Mibew.Objects.ChatPopups['572aa33d4a7bf419'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"572aa33d4a7bf419","url":"\/mibew\/chat?locale=en","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
+
+</body>
+
+
+</html>
