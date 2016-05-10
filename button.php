110eadeba36ba1f4c4d26d494f6cc6673e3836b0
Binh Dang <bdang2012@gmail.com>
2016-05-09 23:04:56 -0700

    update button to include michelle

--- button.php
+++ button.php
@@ -3,11 +3,16 @@
 </head>
 
 <body>
-<h1> Ann Louise Messenger Test </h1>
+
+<h1> Talk to any available operators</h1>
 
 
 <!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en" target="_blank" onclick="Mibew.Objects.ChatPopups['572aa33d4a7bf419'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"572aa33d4a7bf419","url":"\/mibew\/chat?locale=en","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
 
+
+<h1>Talk to Michelle Gock</h1>
+
+<!-- mibew button --><a id="mibew-agent-button" href="http://messengeral.bcspecials.com/mibew/chat?locale=en&amp;group=3" target="_blank" onclick="Mibew.Objects.ChatPopups['5731797735ae7ff9'].open();return false;"><img src="http://messengeral.bcspecials.com/mibew/b?i=mibew&amp;lang=en&amp;group=3" border="0" alt="" /></a><script type="text/javascript" src="http://messengeral.bcspecials.com/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"5731797735ae7ff9","url":"http:\/\/messengeral.bcspecials.com\/mibew\/chat?locale=en&group=3","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"http:\/\/messengeral.bcspecials.com\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
 </body>
 
 
