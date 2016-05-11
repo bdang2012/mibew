26512033bb725b238d38bcc85b821f9057e6acc3
Binh Dang <bdang2012@gmail.com>
2016-05-11 16:14:32 -0700

    add yolanda button

--- button.php
+++ button.php
@@ -8,6 +8,10 @@
 
 <!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en" target="_blank" onclick="Mibew.Objects.ChatPopups['572aa33d4a7bf419'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"572aa33d4a7bf419","url":"\/mibew\/chat?locale=en","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
 
+
+<h1>Talk to Yolanda</h1>
+
+<!-- mibew button --><a id="mibew-agent-button" href="http://messengeral.bcspecials.com/mibew/chat?locale=en&amp;group=4" target="_blank" onclick="Mibew.Objects.ChatPopups['5733bc8dc900a5b2'].open();return false;"><img src="http://messengeral.bcspecials.com/mibew/b?i=mibew&amp;lang=en&amp;group=4" border="0" alt="" /></a><script type="text/javascript" src="http://messengeral.bcspecials.com/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"5733bc8dc900a5b2","url":"http:\/\/messengeral.bcspecials.com\/mibew\/chat?locale=en&group=4","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"http:\/\/messengeral.bcspecials.com\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
 </body>
 
 
