b318952b4cf1449c84cdf98ade1ed53f2f35a903
Binh Dang <bdang2012@gmail.com>
2016-05-18 14:58:33 -0700

    button

--- button.php
+++ button.php
@@ -4,15 +4,30 @@
 
 <body>
 
+<h1>Who is available</h1>
+<table>
+    <th>
+        <td>Michelle Gock</td>
+        <td>Louise Chen</td>
+        <td>Yolanda Delazerda</td>
+
+    </th>
+
+    <tr>
+        <td><!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en&amp;group=3" target="_blank" onclick="Mibew.Objects.ChatPopups['573ce46f8cd6f716'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en&amp;group=3" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"573ce46f8cd6f716","url":"\/mibew\/chat?locale=en&group=3","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button --></td>
+        <td><!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en&amp;group=6" target="_blank" onclick="Mibew.Objects.ChatPopups['573ce492ce0bf2be'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en&amp;group=6" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"573ce492ce0bf2be","url":"\/mibew\/chat?locale=en&group=6","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button --></td>
+        <td><!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en&amp;group=8" target="_blank" onclick="Mibew.Objects.ChatPopups['573ce4d198790bc6'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en&amp;group=8" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"573ce4d198790bc6","url":"\/mibew\/chat?locale=en&group=8","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button --></td>
+    </tr>
+</table>
+
+
+
+
 <h1> Talk to any available operators</h1>
 
 <!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en" target="_blank" onclick="Mibew.Objects.ChatPopups['572aa33d4a7bf419'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"572aa33d4a7bf419","url":"\/mibew\/chat?locale=en","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
 
 
-<h1>Talk to Yolanda</h1>
-
-<!-- mibew button --><a id="mibew-agent-button" href="http://messengeral.bcspecials.com/mibew/chat?locale=en&amp;group=4" target="_blank" onclick="Mibew.Objects.ChatPopups['5733bc8dc900a5b2'].open();return false;"><img src="http://messengeral.bcspecials.com/mibew/b?i=mibew&amp;lang=en&amp;group=4" border="0" alt="" /></a><script type="text/javascript" src="http://messengeral.bcspecials.com/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"5733bc8dc900a5b2","url":"http:\/\/messengeral.bcspecials.com\/mibew\/chat?locale=en&group=4","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"http:\/\/messengeral.bcspecials.com\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
-</body>
 
 <h1>Talk to a specific operator</h1>
 <ul>
@@ -25,4 +40,7 @@
 </ul>
 <p>Binh and Joseph for testing purposes and will be removed</p>
 <!-- mibew operator code field --><form action="" onsubmit="var popup = Mibew.Objects.ChatPopups['573cdcbc1239bb54'];popup.open(popup.buildChatUrl() + '&amp;operator_code=' + document.getElementById('mibew-operator-code-field').value);return false;" id="mibew-operator-code-form"><input type="text" id="mibew-operator-code-field" /></form><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"573cdcbc1239bb54","url":"\/mibew\/chat?locale=en","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew operator code field -->
+
+
+</body>
 </html>
