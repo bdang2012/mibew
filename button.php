769c954fec5e98d288d7fd8ee9e527dd235d7ec4
Binh Dang <bdang2012@gmail.com>
2016-05-18 15:01:24 -0700

    button

--- button.php
+++ button.php
@@ -6,12 +6,12 @@
 
 <h1>Who is available</h1>
 <table>
-    <th>
-        <td>Michelle Gock</td>
-        <td>Louise Chen</td>
-        <td>Yolanda Delazerda</td>
+    <tr>
+        <th>Michelle Gock</th>
+        <th>Louise Chen</th>
+        <th>Yolanda Delazerda</th>
 
-    </th>
+    </tr>
 
     <tr>
         <td><!-- mibew button --><a id="mibew-agent-button" href="/mibew/chat?locale=en&amp;group=3" target="_blank" onclick="Mibew.Objects.ChatPopups['573ce46f8cd6f716'].open();return false;"><img src="/mibew/b?i=mibew&amp;lang=en&amp;group=3" border="0" alt="" /></a><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"573ce46f8cd6f716","url":"\/mibew\/chat?locale=en&group=3","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew button --></td>
