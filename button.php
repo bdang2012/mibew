94e42d3803fa76e2dd70c3ffa0d6714c89ab3c8f
Binh Dang <bdang2012@gmail.com>
2016-05-18 14:28:13 -0700

    button.php operator code field for talking to a specific operator

--- button.php
+++ button.php
@@ -14,5 +14,15 @@
 <!-- mibew button --><a id="mibew-agent-button" href="http://messengeral.bcspecials.com/mibew/chat?locale=en&amp;group=4" target="_blank" onclick="Mibew.Objects.ChatPopups['5733bc8dc900a5b2'].open();return false;"><img src="http://messengeral.bcspecials.com/mibew/b?i=mibew&amp;lang=en&amp;group=4" border="0" alt="" /></a><script type="text/javascript" src="http://messengeral.bcspecials.com/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"5733bc8dc900a5b2","url":"http:\/\/messengeral.bcspecials.com\/mibew\/chat?locale=en&group=4","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"http:\/\/messengeral.bcspecials.com\/mibew\/chat\/style\/popup"});</script><!-- / mibew button -->
 </body>
 
-
+<h1>Talk to a specific operator</h1>
+<ul>
+    <li>Administrator 1</li>
+    <li>Michelle Gock 2</li>
+    <li>Louise Chen 3</li>
+    <li>Yolanda Delazerda 4</li>
+    <li>Joseph Pierre 5</li>
+    <li>Binh Dang 6</li>
+</ul>
+<p>Binh and Joseph for testing purposes and will be removed</p>
+<!-- mibew operator code field --><form action="" onsubmit="var popup = Mibew.Objects.ChatPopups['573cdcbc1239bb54'];popup.open(popup.buildChatUrl() + '&amp;operator_code=' + document.getElementById('mibew-operator-code-field').value);return false;" id="mibew-operator-code-form"><input type="text" id="mibew-operator-code-field" /></form><script type="text/javascript" src="/mibew/js/compiled/chat_popup.js"></script><script type="text/javascript">Mibew.ChatPopup.init({"id":"573cdcbc1239bb54","url":"\/mibew\/chat?locale=en","preferIFrame":true,"modSecurity":false,"width":640,"height":480,"resizable":true,"styleLoader":"\/mibew\/chat\/style\/popup"});</script><!-- / mibew operator code field -->
 </html>
